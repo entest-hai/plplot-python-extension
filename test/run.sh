cbb3469283493150060133511ac5e6c9cda6730c
Tran Minh Hai <hai@bio-rithm.com>
2021-10-09 12:40:56 +0000

    test gs conver to pdf with size

--- test/run.sh
+++ test/run.sh
@@ -2,3 +2,7 @@ rm app
 gcc -o app test_plplot.c -I /usr/local/include/ -I /usr/local/include/plplot/ -lm -lgsl -lgslcblas -lplplot
 ./app
 gs -dSAFER -dBATCH -dNOPAUSE -dEPSCrop -r200 -sDEVICE=png16m -sOutputFile=grid.png grid.ps
+
+gs -dSAFER -dBATCH -dNOPAUSE -dEPSCrop -sDEVICE=pdfwrite -g5775x6207 -dPDFFitPage -o grid.pdf grid.ps
+
+gs -dSAFER -dBATCH -dNOPAUSE -dEPSCrop -sDEVICE=pdfwrite -dPDFFitPage -o grid.pdf grid.ps
