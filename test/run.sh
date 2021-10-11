432769620a7931125f1653e9518e1fc1bd59d3d6
Tran Minh Hai <hai@bio-rithm.com>
2021-10-11 16:06:35 +0700

    add plot simulated heart rate and ua

--- test/run.sh
+++ test/run.sh
@@ -1,8 +1,7 @@
 rm app
-gcc -o app test_plplot.c -I /usr/local/include/ -I /usr/local/include/plplot/ -lm -lgsl -lgslcblas -lplplot
+gcc -o app ./../src/plplot_ctg_paper.c -I /usr/local/include/ -I /usr/local/include/plplot/ -lm -lgsl -lgslcblas -lplplot
 ./app
 gs -dSAFER -dBATCH -dNOPAUSE -dEPSCrop -r200 -sDEVICE=png16m -sOutputFile=grid.png grid.ps
-
 gs -dSAFER -dBATCH -dNOPAUSE -dEPSCrop -sDEVICE=pdfwrite -g5775x6207 -dPDFFitPage -o grid.pdf grid.ps
-
 gs -dSAFER -dBATCH -dNOPAUSE -dEPSCrop -sDEVICE=pdfwrite -dPDFFitPage -o grid.pdf grid.ps
+okular grid.pdf
